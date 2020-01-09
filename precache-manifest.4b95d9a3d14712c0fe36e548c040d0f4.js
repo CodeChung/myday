6536a3462dce1a7a111f58029ba5bf1d23e0b10e
Harry Chung <hc9825@gmail.com>
2020-01-09 15:26:29 -0800

    Updates

---
+++ precache-manifest.4b95d9a3d14712c0fe36e548c040d0f4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effe8550749bca0e6e2656bc0354b606",
+    "url": "/myday/index.html"
+  },
+  {
+    "revision": "c88a1e37e7963cc2dcf2",
+    "url": "/myday/static/css/2.91b209d5.chunk.css"
+  },
+  {
+    "revision": "5fbbcfcda8fea377cd43",
+    "url": "/myday/static/css/main.691ff94f.chunk.css"
+  },
+  {
+    "revision": "c88a1e37e7963cc2dcf2",
+    "url": "/myday/static/js/2.0a8dac37.chunk.js"
+  },
+  {
+    "revision": "5fbbcfcda8fea377cd43",
+    "url": "/myday/static/js/main.f3478a55.chunk.js"
+  },
+  {
+    "revision": "3f46ceec380048cad4f8",
+    "url": "/myday/static/js/runtime-main.9406039c.js"
+  },
+  {
+    "revision": "47fcc02188456a90c8bfb96ec52c8b5b",
+    "url": "/myday/static/media/beach.47fcc021.mp4"
+  },
+  {
+    "revision": "c255198d2faee7a97189a06dc8c187f4",
+    "url": "/myday/static/media/biopic.c255198d.JPG"
+  },
+  {
+    "revision": "bc4e18f71db12d4eb4c03d5b90a04829",
+    "url": "/myday/static/media/candle.bc4e18f7.mp4"
+  },
+  {
+    "revision": "4d2d9ff633beeff03c0fc708b4f21b45",
+    "url": "/myday/static/media/couple.4d2d9ff6.png"
+  },
+  {
+    "revision": "2e9cb156d09d8b7a91a457c725679915",
+    "url": "/myday/static/media/flour.2e9cb156.mp4"
+  },
+  {
+    "revision": "6b5b9ec5ac1cd57a391fb3c5fe1b61a1",
+    "url": "/myday/static/media/incense.6b5b9ec5.mp4"
+  },
+  {
+    "revision": "f1e71d25a2af21973fd789837d91e2a1",
+    "url": "/myday/static/media/lol.f1e71d25.png"
+  },
+  {
+    "revision": "63dfbc89b9c9d2de9f2558e89ce46360",
+    "url": "/myday/static/media/services.63dfbc89.jpg"
+  }
+]);
